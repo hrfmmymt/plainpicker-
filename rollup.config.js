46096d580d83b95f2b81e91429a8cf13f029d7b7
hrfmmymt <hrfmmymt@gmail.com>
2017-06-27 18:41:47 +0900

    modified

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,22 @@
 import eslint from 'rollup-plugin-eslint'
 import babel from 'rollup-plugin-babel'
+import uglify from 'rollup-plugin-uglify'
+import replace from 'rollup-plugin-replace'
 
 export default {
   entry: 'src/js/app.js',
   format: 'cjs',
   dest: 'public/js/bundle.js',
-  sourceMap: 'inline',
+  sourceMap: true,
   plugins: [
     eslint(),
     babel({
       exclude: 'node_modules/**'
-    })
+    }),
+    replace({
+      exclude: 'node_modules/**',
+      ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+    }),
+    (process.env.NODE_ENV === 'production' && uglify())
   ]
 }
